57323bb381b38dd79fc8b17fb24df564757605b9
Ben Reynhout <ben.reynhout@gmail.com>
2015-09-04 15:42:12 -0400

    first real code commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    for x in 1..31 {
+    	if x %3 == 0 && x %5 == 0{
+    		println!("fizzbuzz");
+    	}
+    	else if x %3 == 0{
+    		println!("fizz");
+    	}
+    	else if x %5 == 0{
+    		println!("buzz");
+    	}
+    	else{
+    	println!("{}", x);
+    	}
+    }
 }
